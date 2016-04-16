fe78a2e19f3073bf5048b3a39d3a82a490dadea7
Nelson <cubecnelson>
2016-04-16 23:33:16 +0800

    wishlist is updated

--- client/cheapjing/www/js/app.js
+++ client/cheapjing/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova','starter.controllers', 'ngResource', 'lbServices'])
+angular.module('starter', ['ionic', 'ngCordova','starter.controllers', 'ngResource', 'lbServices', 'starter.services'])
 
 .run(function($ionicPlatform, $rootScope, $state, $location) {
   $ionicPlatform.ready(function() {
