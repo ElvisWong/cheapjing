ce2f274b26b64ce75a150952b107dfb92b6b0982
Elvis Wong <elvis@techpacker.com>
2016-04-17 04:01:12 +0800

    default api created

--- common/models/member.js
+++ common/models/member.js
@@ -5,75 +5,75 @@ var loopback = require('loopback');
 module.exports = function(Member) {
 
 	
-	Member.afterRemote('login', function(context, obj, next){
-		console.log("Login: ", obj);
-		next();
-	});
+	// Member.afterRemote('login', function(context, obj, next){
+	// 	console.log("Login: ", obj);
+	// 	next();
+	// });
 
-	Member.afterRemote('logout', function(context, obj, next){
-		console.log("Logout");
-		next();
-	});
+	// Member.afterRemote('logout', function(context, obj, next){
+	// 	console.log("Logout");
+	// 	next();
+	// });
 
 
-	Member.getMember = function(data, cb){
-		var ctx = loopback.getCurrentContext();
-		// var accessToken = ctx.get('accessToken');
-		// console.log(accessToken);
-    	var currentUser = ctx && ctx.get('currentUser');
-    	// console.log(currentUser);
+	// Member.getMember = function(data, cb){
+	// 	var ctx = loopback.getCurrentContext();
+	// 	// var accessToken = ctx.get('accessToken');
+	// 	// console.log(accessToken);
+ //    	var currentUser = ctx && ctx.get('currentUser');
+ //    	console.log(currentUser);
 
-    	if (currentUser != null){
-    		console.log(currentUser);
-    		cb(null, currentUser);
-    	} else{
-    		var errMsg = "Fail to get current user";
-    		console.log(errMsg);
-    		cb(null, errMsg);
-    	}
-	}
+ //    	if (currentUser != null){
+ //    		console.log(currentUser);
+ //    		cb(null, currentUser);
+ //    	} else{
+ //    		var errMsg = "Fail to get current user";
+ //    		console.log(errMsg);
+ //    		cb(null, errMsg);
+ //    	}
+	// }
 
-	Member.editMember = function(data, cb){
-		var ctx = loopback.getCurrentContext();
-		// var accessToken = ctx.get('accessToken');
-		// console.log(accessToken);
-    	var currentUser = ctx && ctx.get('currentUser');
-    	// console.log(currentUser);
+	// Member.editMember = function(data, cb){
+	// 	var ctx = loopback.getCurrentContext();
+	// 	// var accessToken = ctx.get('accessToken');
+	// 	// console.log(accessToken);
+ //    	var currentUser = ctx && ctx.get('currentUser');
+ //    	// console.log(currentUser);
 
-    	if (currentUser != null){
-    		Member.findById(currentUser.id, function(err, mem){
-    			if (err){
-    				console.log(err);
-    				cb(err, null);
-    			} else{
-	    			mem.updateAttributes(data, function(err, mem){
-	    				if (err){
-		    				console.log(err);
-		    				cb(err, null);
-		    			} else{
-		    				console.log("Edit Member: ", mem);
-		    				cb(null, mem);
-		    			}
-	    			});
-	    		}
-    		});
-    	} else{
-    		var errMsg = "Fail to get current user";
-    		console.log(errMsg);
-    		cb(null, errMsg);
-    	}
-	}
+ //    	if (currentUser != null){
+ //    		Member.findById(currentUser.id, function(err, mem){
+ //    			if (err){
+ //    				console.log(err);
+ //    				cb(err, null);
+ //    			} else{
+	//     			mem.updateAttributes(data, function(err, mem){
+	//     				if (err){
+	// 	    				console.log(err);
+	// 	    				cb(err, null);
+	// 	    			} else{
+	// 	    				console.log("Edit Member: ", mem);
+	// 	    				cb(null, mem);
+	// 	    			}
+	//     			});
+	//     		}
+ //    		});
+ //    	} else{
+ //    		var errMsg = "Fail to get current user";
+ //    		console.log(errMsg);
+ //    		cb(null, errMsg);
+ //    	}
+	// }
 
-	Member.remoteMethod('getMember', {
-		http: {path: '/getMember', verb: 'post'},
-		accepts: {arg: 'data', type: 'object', http:{source:'body'}},
-		returns: {arg: 'member', type: 'object'}
-	});
+	// Member.remoteMethod('getMember', {
+	// 	http: {path: '/getMember', verb: 'post'},
+	// 	accepts: {arg: 'data', type: 'object', http:{source:'body'}},
+	// 	returns: {arg: 'member', type: 'object'}
+	// });
 
-	Member.remoteMethod('editMember', {
-		http: {path: '/editMember', verb: 'post'},
-		accepts: {arg: 'data', type: 'object', http:{source:'body'}},
-		returns: {arg: 'member', type: 'object'}
-	});
+	// Member.remoteMethod('editMember', {
+	// 	http: {path: '/editMember', verb: 'post'},
+	// 	accepts: {arg: 'data', type: 'object', http:{source:'body'}},
+	// 	returns: {arg: 'member', type: 'object'}
+	// });
 
 };
--- server/server.js
+++ server/server.js
@@ -17,27 +17,27 @@ app.start = function() {
   });
 };
 
-// pre-processing middleware
-app.use(loopback.context());
-app.use(loopback.token());
-app.use(function setCurrentUser(req, res, next) {
-  if (!req.accessToken) {
-    return next();
-  }
-  app.models.Member.findById(req.accessToken.userId, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(new Error('No user with this access token was found.'));
-    }
-    var loopbackContext = loopback.getCurrentContext();
-    if (loopbackContext) {
-      loopbackContext.set('currentUser', user);
-    }
-    next();
-  });
-});
+// // pre-processing middleware
+// app.use(loopback.context());
+// app.use(loopback.token());
+// app.use(function setCurrentUser(req, res, next) {
+//   if (!req.accessToken) {
+//     return next();
+//   }
+//   app.models.Member.findById(req.accessToken.userId, function(err, user) {
+//     if (err) {
+//       return next(err);
+//     }
+//     if (!user) {
+//       return next(new Error('No user with this access token was found.'));
+//     }
+//     var loopbackContext = loopback.getCurrentContext();
+//     if (loopbackContext) {
+//       loopbackContext.set('currentUser', user);
+//     }
+//     next();
+//   });
+// });
 
 
 // Bootstrap the application, configure models, datasources and middleware.
