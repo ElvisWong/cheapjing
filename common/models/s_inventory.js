92e067d2bfd8c8418ca8478aa4ea07eee638f12e
Elvis Wong <elvis@techpacker.com>
2016-04-17 01:08:01 +0800

    to josie

--- common/models/s_inventory.js
+++ common/models/s_inventory.js
@@ -1,3 +1,13 @@
+var path = require('path');
+var app = require(path.resolve(__dirname, '../../server/server'));
+var loopback = require('loopback');
+
 module.exports = function(s_inventory) {
+
+	s_inventory.create(data, function(err, s_inventory) {
+		if (err) {
+			console.log(err);
+		}
+	});
 
 };
